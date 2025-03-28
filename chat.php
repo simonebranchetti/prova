f1535f21eb4447b496430ff7ec6cdaff69449ae7
simonebranchetti <145555409+simonebranchetti@users.noreply.github.com>
2025-03-28 13:45:58 +0100

    Add files via upload

--- chat.php
+++ chat.php
@@ -3,7 +3,6 @@
 $username = $_POST["username"];
 $password = $_POST["password"];
 $email = $_POST["email"];
-
 $host = "127.0.0.1";
 $user = "root";
 $rpassword = "";
@@ -11,10 +10,10 @@ $nomedb = "chat";
 
 
 $connessione = new mysqli($host , $user , $rpassword , $nomedb);
-
+var_dump($connessione);
 if ( $connessione === false ){
 
-    echo("connessione non riuscita" . $connessione -> connect_errror);
+    echo("connessione non riuscita" . $connessione->connect_error);
     die();
 }
 
@@ -23,7 +22,30 @@ else{
     echo ("connessione riuscita" . $connessione -> host_info);
 }
 
-$sql = "insert into user(nickname ,user_password , user_email) values ($username , $password , $email)";
+
+$sql = "insert into user(nickname ,user_pass , user_email) values ('$username', '$password' , '$email')";
+$result = $connessione ->query($sql);
+echo("dopo esecuzione query");
+var_dump($result);
+
+if($result){
+    echo($result -> affected_rows . " riga inserita ");
+
+}
+
+else{
+
+    echo("errore");
+}
+
+$sql = "select * from user";
+$result = $connessione ->query($sql);
+var_dump($result);
+if($result){
+    while ($row = $result->fetch_assoc()) { //fetch -> prendere i valori di result (righe)
+        printf("%s (%s)\n", $row["nickname"], $row["user_email"]);
+    }
+}
 
 
 ?>
